fd77961751eb5259eccad283b07131c6f1393712
Dmitry <59611455+KornDV@users.noreply.github.com>
2020-07-11 02:28:16 -0400

    Add files via upload

---
+++ django_server.py
@@ -0,0 +1,93 @@
+#!/bin/python3
+import os
+import sys
+import getpass
+print('\nHello! Let`s make server for Django project!\n')
+project_name=input('Project name: ')
+user_name=getpass.getuser()
+print('\n--------------------------------------------------')
+print('now instal: '+str(project_name)+'\nfor: '+str(user_name))
+print('--------------------------------------------------\n')
+print('All OK?')
+q=input('Yes/No: ')
+if q not in ['Yes','YES','yes','Y','y']:
+	exit()
+
+now_command='sudo mkdir /var/site; sudo apt-get update; sudo apt-get install -y python3-pip python3-venv nginx'
+os.system(now_command)
+
+now_command='sudo python3 -m venv /var/site/env'
+os.system(now_command)
+
+script_1='#!/bin/bash\nsource /var/site/env/bin/activate\npip3 install Django\ncd /var/site\ndjango-admin startproject '+str(project_name)+'\npip3 install gunicorn'
+with open('script_1.sh', 'w') as f:
+	f.write(script_1)
+
+now_command='chmod +x script_1.sh; ./script_1.sh; rm script_1.sh'
+os.system(now_command)
+
+start_gunicorn='#!/bin/bash\nsource /var/site/env/bin/activate\nexec gunicorn -c "/var/site/'+str(project_name)+'/gunicorn_config.py" '+str(project_name)+'.wsgi'
+with open('/var/site/'+str(project_name)+'/start_gunicorn.sh', 'w') as f:
+        f.write(start_gunicorn)
+
+now_command='chmod +x /var/site/'+str(project_name)+'/start_gunicorn.sh'
+os.system(now_command)
+
+gunicorn_config="command = '/var/site/env/bin/gunicorn'\n"
+gunicorn_config+="pythonpath='/var/site/"+str(project_name)+"/"+str(project_name)+"/'\n"
+gunicorn_config+="bind = '127.0.0.1:8001'\nworkers = 3\n"
+gunicorn_config+="user = '"+str(user_name)+"'\n"
+gunicorn_config+="limit_request_fields = 32000\nlimit_request_field_size = 0\nraw_env = 'DJANGO_SETTINGS_MODULE="+str(project_name)+".settings'"
+
+with open('/var/site/'+str(project_name)+'/gunicorn_config.py', 'w') as f:
+        f.write(gunicorn_config)
+
+nginx_config='''server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+
+        location /static/ {
+                root /var/site/'''+str(project_name)+'''/;
+        }
+
+        location / {
+                proxy_pass http://127.0.0.1:8001;
+                proxy_set_header X-Forwarded-Host $server_name;
+                proxy_set_header X-Real-IP $remote_addr;
+                add_header P3P 'CP="ALL DSP COR PSAa PSDa OUR NOR ONL UNI COM NAV"';
+                add_header Access-Conrol-Allow-Origin *;
+        }
+}'''
+with open('/etc/nginx/sites-enabled/default', 'w') as f:
+        f.write(nginx_config)
+
+now_command='sudo service nginx restart'
+os.system(now_command)
+
+
+guni_service = '''[Unit]
+Descriptiom=Guni
+
+[Service]
+User='''+str(user_name)+'''
+WorkingDirectory=/var/site/'''+str(project_name)+'''/
+VIRTUAL_ENV=/var/site/env
+Environment=PATH=$VIRTUAL_ENV/bin:$PATH
+
+ExecStart=/var/site/env/bin/gunicorn -c /var/site/'''+str(project_name)+'''/gunicorn_config.py '''+str(project_name)+'''.wsgi
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target'''
+
+with open('/etc/systemd/system/guni.service', 'w') as f:
+        f.write(guni_service)
+
+now_command='systemctl start guni.service; systemctl enable guni.service'
+os.system(now_command)
